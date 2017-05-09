f41d62240dc2cc11a668e30520ba05a6a841c23e
zongzhang <zhucezhuanyong_007@163.com>
2017-05-09 23:51:06 +0800

    单件模式

--- src/main/java/com/zongzhang/Singleton/Singleton.java
+++ src/main/java/com/zongzhang/Singleton/Singleton.java
@@ -4,7 +4,11 @@ package com.zongzhang.Singleton;
  * @author by zongzhang on 2017/5/8.
  */
 public class Singleton {
-    private static Singleton singleton = new Singleton();
+    // volatile 1.5以上
+    private volatile static Singleton singleton = new Singleton();
+
+    private Singleton() {
+    }
 
     public static Singleton getInstence() {
         return singleton;
