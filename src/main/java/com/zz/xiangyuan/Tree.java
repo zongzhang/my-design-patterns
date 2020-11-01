05fcdf075b22cda2f4db5ee83b140e1ff3d958fd
zongzhang <jingchang_007@163.com>
2020-11-01 18:02:50 +0800

    享元模式

---
+++ src/main/java/com/zz/xiangyuan/Tree.java
@@ -0,0 +1,22 @@
+package com.zz.xiangyuan;
+
+import java.awt.*;
+
+/**
+ * @since 1.0
+ */
+public class Tree {
+    private int x;
+    private int y;
+    private TreeType type;
+
+    public Tree(int x, int y, TreeType type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public void draw(Graphics g) {
+        type.draw(g, x, y);
+    }
+}
